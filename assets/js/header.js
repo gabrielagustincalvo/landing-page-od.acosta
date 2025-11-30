a20b648268a630daff1c396d067e3a89cbf91353
Gabriel Agustin Calvo <gabrielagustincalvo@gmail.com>
2025-11-30 10:59:38 -0300

    Trabaje sobre mejoras en el header.html, footer.html y tambien en estilos.css

--- assets/js/header.js
+++ assets/js/header.js
@@ -104,6 +104,38 @@ export function cargarHeader(){
 
         .then(htmlContent => {
             headerContainer.innerHTML = htmlContent
+
+            // 1. Dónde estoy parado ahora (Ej: /tratamientos/bruxismo.html)
+            const currentPath = window.location.pathname;
+                
+            // 2. Buscamos SOLO los enlaces principales del menú (nav-link)
+            const navLinks = headerContainer.querySelectorAll('.nav-link');
+
+            navLinks.forEach(link => {
+                const linkHref = link.getAttribute('href');
+
+                if (!linkHref) return; // Si el link está vacío, ignorar.
+
+                // CASO A: Coincidencia Exacta
+                // Sirve para Inicio, Acerca de mí, Contacto.
+                // Ej: currentPath es "/pages/contacto.html" y el link es "/pages/contacto.html"
+                if (currentPath === linkHref) {
+                    link.classList.add('active');
+                }
+                    
+                // CASO B: Página de Inicio (Raíz)
+                // Si estás en midominio.com/ y el link es index.html
+                else if (currentPath === '/' && linkHref.includes('index.html')) {
+                    link.classList.add('active');
+                }
+
+                // CASO C: Sección TRATAMIENTOS
+                // Si la URL actual tiene la palabra "tratamientos" (ej: /tratamientos/bruxismo.html)
+                // Y el botón del menú también lleva a "tratamientos"
+                else if (currentPath.includes('tratamientos') && linkHref.includes('tratamientos')) {
+                    link.classList.add('active');
+                }
+            });
         })
         .catch(err => console.error('Error cargando el header', err));
     })
